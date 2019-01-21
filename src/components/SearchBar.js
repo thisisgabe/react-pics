8a0b9a3798bc471e203a0697e2969582bd2b55fd
Gabe Velasquez <gvelasquez@trackforce.com>
2019-01-21 15:00:26 -0800

    event handlers on elements

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  onInputClick() {
+    console.log("input was clicked");
+  }
+
   render() {
-    return <div>Search bar</div>;
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              onClick={this.onInputClick}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
