7c4723e6a79b03546ba865f8485a1e5416bd80e7
Gabe Velasquez <gvelasquez@trackforce.com>
2019-01-22 09:37:52 -0800

    working axios GET request

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import axios from "axios";
+
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID 317f1f38d2aba37b609f20a2a28f1211635f034b3383f4f6d51c4feb63b37971"
+      }
+    });
   }
 
   render() {
