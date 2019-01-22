fa776c55e961b1b4720df5cb07767e92fa2ab298
Gabe Velasquez <gvelasquez@trackforce.com>
2019-01-22 13:09:42 -0800

    finished image app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import unsplash from "../api/unsplash";
-
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
@@ -14,7 +14,6 @@ class App extends React.Component {
       }
     });
 
-    console.log(this);
     this.setState({ images: response.data.results });
   };
 
@@ -22,7 +21,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
